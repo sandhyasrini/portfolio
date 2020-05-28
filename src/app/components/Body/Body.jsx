33696910eace8d769a5aae0cd0bd272995ec7e4c
Sandhya Srinivasan <sandhya.srinivasan@lloydsbanking.com>
2020-05-28 14:39:41 +0100

    image changed

--- src/app/components/Body/Body.jsx
+++ src/app/components/Body/Body.jsx
@@ -12,13 +12,28 @@ class Body extends Component {
             this.setState({left: e.pageX, top: e.pageY});
         });
     }
+
+    changeText() {
+        
+        const firstText = "a Full stack enthusiast!";
+        let displayText;
+        let length = firstText.length;
+            while(length != 0) 
+            {
+                displayText = firstText.substring(0, length);
+                length--;
+            }
+            return displayText;        
+    };
+
+    
     render() {
         return (
             <section className="body-background">
                 <div style={{left: this.state.left, top: this.state.top}} className="cursor-pointer"></div>
                 <div className="center">
                 <p className="title">Hello I am <span className="name">Sandhya Srinivasan</span>,</p>
-                <p className="title">a Full stack enthusiast!</p>
+                <p className="title">{this.changeText()}</p>
                 </div>
                 </section>
         )
