11d756826defaf4f8b1f76401eecb5c2fa24284e
Sandhya Srinivasan <sandhya.srinivasan@lloydsbanking.com>
2020-05-28 10:05:09 +0100

    custom cursor addition

--- src/app/components/Body/Body.jsx
+++ src/app/components/Body/Body.jsx
@@ -2,9 +2,20 @@ import React, {Component} from 'react'
 import '../../assets/styles/Body.less';
 
 class Body extends Component {
+    state = {
+        left: 0,
+        top: 0
+    }
+    componentDidMount() {
+        let cursor = document.getElementById('cursor');
+        document.addEventListener('mousemove', (e) => {
+            this.setState({left: e.pageX, top: e.pageY});
+        });
+    }
     render() {
         return (
             <section className="body-background">
+                <div style={{left: this.state.left, top: this.state.top}} className="cursor-pointer"></div>
                 <div className="center">
                 <p className="title">Hello I am <span className="name">Sandhya Srinivasan</span>,</p>
                 <p className="title">a Full stack enthusiast!</p>
